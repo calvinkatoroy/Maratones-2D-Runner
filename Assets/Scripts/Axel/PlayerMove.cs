1e41d108d338957196385158b6e73d602f866106
AxelKalembang <149135032+AxelKalembang@users.noreply.github.com>
2024-12-22 07:53:45 +0700

    Menghapus folder embedded repository dari staging

--- Assets/Scripts/Axel/PlayerMove.cs
+++ Assets/Scripts/Axel/PlayerMove.cs
@@ -1,10 +1,11 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
@@ -17,7 +18,14 @@ public class PlayerMove : MonoBehaviour
     }
 
  void Update()
-{
+{   
+    //jump
+    if (Input.GetButtonDown("Jump")){
+        rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        anim.SetBool("isJumping", true);
+    }
+        
+
     //Stop Speed
     if (Input.GetButtonUp("Horizontal")) {
         rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
@@ -45,5 +53,17 @@ public class PlayerMove : MonoBehaviour
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         else if (rigid.velocity.x < maxSpeed * (-1)) //Left Max Speed
             rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+
+        //Landing Platform
+        if(rigid.velocity.y < 0){
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0,1,0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if(rayHit.collider != null){
+            if(rayHit.distance < 0.5f)
+                anim.SetBool("isJumping", false);
+        
+        }
+        }
     }
 }
